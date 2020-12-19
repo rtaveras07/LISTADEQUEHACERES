ccb8cb731711d6d233957484e33ce92752627791
rtaveras07 <rafaelvtaveras@gmail.com>
2020-12-18 23:44:04 -0400

    version validacion

--- app.js
+++ app.js
@@ -5,60 +5,50 @@ document.getElementById('Formulario_tareas').addEventListener('submit', salvarTa
 function salvarTarea() {
     let tarea = document.getElementById('entradaTarea').value;
     //se optiene la entrada de la tarea del texto a una variable local tarea 
+if(tarea.length==0){
+alert('Nada que agregar!');
+
+}else {
+
    
-   
-   
-   
-    //se almacena en un objeto la variable anterior con la tarea. 
-   
-   
-   // const objetoTarea = {
-
-     //   tarea
-
-//};
-
-
-
-
-
 //si no hay tareas en el local store
 if(localStorage.getItem('tareas') === null) {
-    let tareas = [];//se inicializa el arreglo  tareas 
-    tareas.push(tarea);//se agrega al arreglo tarea el item. 
-    localStorage.setItem('tareas', JSON.stringify(tareas));// agrega la tarea al storage en formato json
-  } else { // si ya exitiese alguna tarea 
-    let tareas = JSON.parse(localStorage.getItem('tareas'));//se agrega la tarea  
-    tareas.push(tarea);//se agrega al arreglo 
-    localStorage.setItem('tareas', JSON.stringify(tareas)); //se almacena 
-  }
-
-  getTareas();
-  document.getElementById('Formulario_tareas').reset();
-
+  let tareas = [];//se inicializa el arreglo  tareas 
+  tareas.push(tarea);//se agrega al arreglo tarea el item. 
+  localStorage.setItem('tareas', JSON.stringify(tareas));// agrega la tarea al storage en formato json
+} else { // si ya exitiese alguna tarea 
+  let tareas = JSON.parse(localStorage.getItem('tareas'));//se agrega la tarea  
+  tareas.push(tarea);//se agrega al arreglo 
+  localStorage.setItem('tareas', JSON.stringify(tareas)); //se almacena 
 }
+
+getTareas();
+document.getElementById('Formulario_tareas').reset();
+ 
+}
+ 
+ 
+}
+
+
+ 
 
 
 
 function eliminarTarea(elementoEliminar) {
   console.log(elementoEliminar);
   eliminar=confirm("¿Deseas eliminar este registro?");
-    if (eliminar)
-     
+    if (eliminar)   
   
-          {
-
-  
+          {  
             let arreglo ='';
             arreglo = JSON.parse(localStorage.getItem('tareas'));
           for(let i = 0; i < arreglo.length; i++) {
             
             if(arreglo[i]== elementoEliminar) {
-              arreglo.splice(i,1);
-   
+              arreglo.splice(i,1); 
             
             }
-
 
           }
           
@@ -72,8 +62,6 @@ function eliminarTarea(elementoEliminar) {
   function getTareas() {  ///opteniendo los items. 
     let tareas = JSON.parse(localStorage.getItem('tareas'));
     let tareasView = document.getElementById('descripcionTarea');
- if (tareasView !=null || ''){
-
     tareasView.innerHTML = '';
     for(let i = 0; i < tareas.length; i++) {//recorriendo el arreglo 
       let listaTareas = tareas[i]
@@ -84,7 +72,7 @@ function eliminarTarea(elementoEliminar) {
         </div>
         `;         
     }
-  }  
+   
 }
   getTareas();
   
